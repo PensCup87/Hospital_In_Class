626daa092892e8067de634d2a4bf6d2e81f19a72
PensCup87 <bdt9123@gmail.com>
2017-10-12 13:58:36 -0400

    created gneral employee and 3 classes

---
+++ Hospital_In_Class/HospitalEmployee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_In_Class
+{
+    class HospitalEmployee
+    {
+        protected string employeeName;
+        protected int employeeNumber;
+
+        public string EmployeeName
+        {
+            get { return this.employeeName; }
+           // set { this.employeeName = value; }
+        }
+
+        public int EmployeeNumber
+        {
+            get { return this.employeeNumber; }
+            //set { this.employeeNumber = value; }
+        }
+
+        public HospitalEmployee()
+        {
+
+        }
+
+        public HospitalEmployee(string employeeName, int employeeNumber)
+        {
+            this.employeeName = employeeName;
+            this.employeeNumber = employeeNumber;
+        }
+            
+    }
+}
